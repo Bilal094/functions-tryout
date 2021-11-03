c16cd5c884017b5caa5cce5bf6da736c3c8bbb4d
Bilal094 <99066318@mydavinci.nl>
2021-11-03 11:45:28 +0100

    Game refactored met functies

---
+++ gameAangepast.py
@@ -0,0 +1,290 @@
+from time import sleep
+Munten = 25
+Health = 100
+def foutkeuze():
+    print('Type a.u.b een gegeven keuze')
+
+def win():
+    print('Je hebt gewonnen!')
+
+def lose():
+    print('Je bent doodgegaan!')
+
+def intro():
+    print('---------------------------------------------------------------------------------------------')
+    print('| Als toerist lijkt je het leuk om op trip te gaan naar de Sahara-woestijn in Noord-Afrika. |')
+    print('| Maar plotseling weet je je weg niet meer terug te vinden door de hitte van de zon...      |')
+    print('| Je raakt in paniek maar tegelijkertijd denk je ook dat paniek het maar erger maakt en je  |')
+    print('| denkt dat je de weg wel terug zal vinden. Je hebt een grote rugzak met daarin 3 flessen   |')
+    print('| water en je drink er 1 op wegens de dorst die door de hitte is veroorzaakt. Daarnaast heb |')
+    print('| je een tent bij je en een slaapzak. Het is op dit moment het heetste moment van de dag... |')
+    print('---------------------------------------------------------------------------------------------')
+    sleep(10)
+    print('Level-1: Je loopt al een eindje door het zand en er blijkt geen hoop te zijn. Wat doe je nu?')
+    sleep(2.5)
+
+intro()
+print('1: Je loopt nog een eindje door in de hoop dat je misschien een klein onderdakje tegenkomt')
+print('2: Je besluit om op een kleine rots te gaan zitten en uit te rusten')
+
+lvl1 = input('Type je keuze door alleen het cijfer in te typen ')
+sleep(0.5)
+if lvl1 == '1':
+    print('Level-2: Je hebt een grot gevonden die naar een onbekende plek leidt')
+    sleep(1)
+    print('1: Je gaat er naar binnen om te verkennen')
+    print('2: Je bent onzeker en je gebruikt je tent om te schuilen van de zon')
+    lvl2 = input('')
+    if lvl2 == '1':
+        sleep(0.5)
+        print('Level-2: Het is veelste donker om te kunnen zien dus je gebruikt je zaklamp')
+        sleep(1)
+        print('Je schijnt je zaklamp op een reuze, giftige zandadder!')
+        sleep(0.5)
+        print('Health = 100')
+        print('1: Doodt het met een steen')
+        print('2: Loop eromheen')
+        lvl3 = input('')
+        if lvl3 == '1':
+            print('Je pakt een grote steen en plet de slang ermee')
+            sleep(1)
+            print('Level-3: Je loopt verder en ziet vele gangen en je hebt de angst om verdwaald te raken. Je maakt een keuze tussen 3 gangen')
+            sleep(1.3)
+            print('1: De linkergang')
+            print('2: De middelste gang')
+            print('3: De rechtergang')
+            lvl4 = input('')
+            if lvl4 == '1':
+                print('De linkergang loopt dood')
+            elif lvl4 == '2':
+                print('Level-4: Je komt een magiër tegen en hij biedt je een dolk aan tegen 10 munten. Je hebt 25 munten.')
+                sleep(1)
+                print('1: Koop de dolk')
+                lvl5 = (input(''))
+                if Munten == 25:
+                    Munten = 25 - 10
+                    print('Level-5: Je bent nu in bezit van een dolk en je wist een jager schorpioen te doden.')
+                    sleep(1.5)
+                    print('Na een tijdje doorgebracht te hebben in de grot, begin je honger te krijgen')
+                    sleep(1.5)
+                    print('Je komt erachter dat je geen proviand bij je hebt')
+                    sleep(1.5)
+                    print('Je hebt nu een keuze om uit de grot te gaan om voedsel te zoeken')
+                    sleep(1.5)
+                    print('...of je blijft in de grot voor je eigen veiligheid en misschien vind je wel voedsel in de grot')
+                    sleep(2)
+                    print('1: Blijf in de grot')
+                    print('2: Ga de grot uit om voedsel te vinden')
+                    lvl6 = input('')
+                    if lvl6 == '1':
+                        print('Je hebt besloten om in de grot te blijven')
+                        sleep(1.8)
+                        print('Je hebt na een lange tijd geprobeerd om voedsel te vinden om je honger te stillen')
+                        sleep(1)
+                        print('Het was geen succes...')
+                        sleep(1)
+                        lose()
+                    elif lvl6 == '2':
+                        print('Je vindt een ander uitweg uit de grot en je ziet in de verte...')
+                        sleep(1.5)
+                        print('...een oase! Perfect om te overleven...')
+                        sleep(1.5)
+                        print('...en je komt een handelaar tegen met een mooie aanbod...')
+                        sleep(1.5)
+                        print('De handelaar biedt je veedieren aan zodat je een bron hebt van vlees en melk')
+                        sleep(1.5)
+                        print('of je maakt je voedsel door zelf zaadjes te planten')
+                        sleep(1.5)
+                        print('De kosten zijn 20 munten. Je hebt 15 munten')
+                        sleep(1.5)
+                        print('1: Schaf de veedieren aan')
+                        lvl7 = input('')
+                        if Munten >= 20:
+                            print('Je hebt de veedieren aangeschaft en je hebt een kleine boerderij opgezet!')
+                            sleep(1,5)
+                            win()
+                        else:
+                            print('Je saldo is helaas lager dan 20 munten en je moest de aanbod afslaan')
+                            sleep(1.5)
+                            print('Level-8: Terwijl je de oase aan het verkennen bent, kom je vijandige buren tegemoet.')
+                            sleep(1.5)
+                            print('1: Probeer te onderhandelen')
+                            print('2: Gebruik je dolk')
+                            lvl8 = input('')
+                            if lvl8 == '1':
+                                print('Je probeert te onderhandelen maar de vijandige nomaden zijn er niet van gediend')
+                                sleep(1.5)
+                                lose()
+                            elif lvl8 == '2':
+                                print('Je gebruikt je dolk die je had gekocht')
+                                sleep(1.5)
+                                print('Je verwondt er één en je bent gestoken in je been')
+                                sleep(1.5)
+                                Health = 100 - 40
+                                print('Health = '+ str(Health) +' Level-9')
+                                print('1: Gebruik een pijlenboog die je op de grond ziet')
+                                print('2: Gebruik je dolk')
+                                lvl9 = input('')
+                                if lvl9 == '1':
+                                    print('Je grijpt naar de pijlenboog van de grond')
+                                    print('Je schiet er 2 neer en verjaagt de rest')
+                                    print('De oase is nu vrij van elk gevaar')
+                                    sleep(1.5)
+                                    print('De oase ligt toevallig naast een handelsroute - iets waarvan jij gebruik van kan maken!')
+                                    sleep(1.5)
+                                    win()
+                                elif lvl9 == '2':
+                                    Health = 60 - 60
+                                    print('Je gebruikt je dolk maar die breekt in het midden van de gevecht')
+                                    sleep(2)
+                                    print('Health = '+ str(Health) +', game over!')
+                            else:
+                                foutkeuze()
+                else:
+                    print('Je hebt de aanbod afgewezen, of je hebt te weinig munten.')
+                    sleep(1.5)
+                    print('Level-6: Je loopt verder en je komt weer een zandadder tegen!')
+                    sleep(1.5)
+                    print('1: Spring erover heen')
+                    print('2: Probeer de slang te grijpen en ermee te slingeren')
+                    lvl6 = input('')
+                    if lvl6 == '1':
+                        print('Je probeert eroverheen te springen maar de adder weet jou net te bijten')
+                        sleep(1.5)
+                        lose()
+                    elif lvl6 == '2':
+                        print('Je probeert de adder te grijpen...')
+                        sleep(1.5)
+                        print('...maar bijt jou voordat je je handen op hem legt')
+                        sleep(1.5)
+                        lose()
+                    else:
+                        foutkeuze()
+            elif lvl4 == '3':
+                print('In de rechtergang nestelt zich een zaagchubadder, maar je wist ze te vermijden')
+                sleep(1.5)
+                print('Voordat je dieper in gaat, zie je een gat boven je, maar je kunt daar niks zien door de stralen van de zon')
+                sleep(1.5)
+                print('Level-4: Je vermoedt dat er mensen boven zijn, omdat je geklets en menselijke activiteiten hoort')
+                print('1: Ga dieper de grot in')
+                print('2: Klim uit het gat')
+                lvl5 = input('')
+                if lvl5 == '1':
+                    print('Je denkt niet dat het verstandig om uit het gat te klimmen...')
+                    sleep(1.5)
+                    print('Je loopt door en je gaat maar dieper... dieper...')
+                    sleep(1.5)
+                    print('...totdat je de weg kwijt bent en verder niks kan doen dan alleen op de dood wachten')
+                    sleep(1.5)
+                    lose()
+                elif lvl5 == '2':
+                    print('Je klimt uit het gat...')
+                    sleep(1.5)
+                    print('...en het is waar: je bent in een dorp terechtgekomen!')
+                    sleep(1.5)
+                    print('Je hebt geen geld en je wilt ervoor gaan werken')
+                    sleep(1.5)
+                    print('Je ziet dat het dorp soldaten nodig heeft en je vindt jezelf geschikt')
+                    print('1: Meld jezelf aan')
+                    lvl6 = input('')
+                    if Health == 100 and Munten >= 20:
+                        print('Je bent aangenomen en je hebt een goed salaris en je kunt daarmee in het dorp wonen!')
+                        sleep(1.5)
+                        win()
+                    else:
+                        print('Je hebt te weinig geld en/of je gezondheid is niet in orde, dus je werkt verder als een knecht en verdient bijna niets')
+                        sleep(2)
+                        lose()
+                else:
+                    foutkeuze()
+        elif lvl3 == '2':
+            print('Je probeert eromheen te lopen maar de slang was sneller en was in staat om jou te bijten en te doden')
+            from time import sleep
+            sleep(1)
+            lose()
+        else:
+            foutkeuze()
+    elif lvl2 == '2':
+        print('Je pakt je tent en zet hem op. Al gauw wordt het donker, dus je pakt ook je slaapzak erbij')
+        from time import sleep
+        sleep(2)
+        print('Het begint ook tegelijkertijd te gaan regenen, maar dat baart geen zorgen bij jou')
+        from time import sleep
+        sleep(2)
+        print('Je sluit je ogen terwijl het buiten de tent spettert met regen')
+        from time import sleep
+        sleep(2.5)
+        print('Verdrinking is de meest voorkomende doodsoorzaak in een woestijn, iets waar jij over zou twijfelen')
+        from time import sleep
+        sleep(2.5)
+        print('Het begint langzaam te vloeden terwijl jij niks doorhebt en jij aan het pitten bent...')
+        from time import sleep
+        sleep(2.5)
+        print('Het water reikt jouw mond en dringt zo je longen in en je \'verdrinkt\' in een zandwoestijn...')
+        from time import sleep
+        sleep(3)
+        lose()
+    else:
+        foutkeuze()
+elif lvl1 == '2':
+    from time import sleep
+    sleep(0.5)
+    print('Level-2: Je krijgt het veelste warm van de zon en je hebt weer dorst')
+    from time import sleep
+    sleep(1.5)
+    print('1: Drink nog 1 fles')
+    print('2: Volhouden en hopen dat de dorst vanzelf verdwijnt')
+    lvl2 = input('')
+    if lvl2 == '1':
+        print('Terwijl jij naar een fles aan het zoeken bent in je rugzak...')
+        from time import sleep
+        sleep(1.5)
+        print('...verschijnt er ineens een gestreepte hyena achter je rug om!')
+        from time import sleep
+        sleep(1.5)
+        print('Level-2: Wat doe je nu?')
+        print('1: Vecht met de hyena')
+        print('2: Doe alsof je dood bent')
+        print('3: Schreeuw om hulp in een leeg woestijn')
+        lvl3 = input('')
+        if lvl3 == '1':
+            print('Je dacht dapper te zijn om tegen de hyena te vechten...')
+            from time import sleep
+            sleep(1.5)
+            print('...maar de gestreepte hyena is sterker dan jij en wint de duel')
+            from time import sleep
+            sleep(1.5)
+            lose()
+        elif lvl3 == '2':
+            print('Je valt neer en doet alsof je dood bent')
+            from time import sleep
+            sleep(1.5)
+            print('maar de hyena trapt er niet en verscheurt je  helemaal total-loss')
+            from time import sleep
+            sleep(1.5)
+            lose()
+        elif lvl3 == '3':
+            print('Uit angst schreeuw je het allemaal uit')
+            from time import sleep
+            sleep(1.5)
+            print('Zo hard dat er nomaden die in de buurt zitten het horen')
+            from time import sleep
+            sleep(1.5)
+            print('Al gauw zien ze jou, doden de hyena en vangen ze jou open en je bent gered!')
+            from time import sleep
+            sleep(1.5)
+            win()
+        else:
+            foutkeuze()
+    elif lvl2 == '2':
+        print('Je probeert het nog even vol te houden...')
+        from time import sleep
+        sleep(1.5)
+        print('...totdat je neervalt van uitdroging')
+        from time import sleep
+        sleep(1.5)
+        lose()
+    else:
+        foutkeuze()
+else:
+    foutkeuze()
